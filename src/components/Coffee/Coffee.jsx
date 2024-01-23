332c6582f4353a4f30d0e65a84faf9b9fbba53c7
Shariful4447 <shariful.islam4447@gmail.com>
2024-01-23 10:41:54 +0100

    coffe page design done

--- src/components/Coffee/Coffee.jsx
+++ src/components/Coffee/Coffee.jsx
@@ -3,8 +3,8 @@ const Coffee = ({coffee}) => {
     const {name, quatity, supplier, taste, category, details, image} = coffee;
     return (
         <div>
-            <div className="card card-side bg-base-100 shadow-xl">
-                <figure className="max-w-96"><img src={image} alt="Movie"/></figure>
+            <div className="card card-side bg-base-100 shadow-xl max-h-96">
+                <figure className="max-w-64"><img src={image} alt="Movie"/></figure>
                 <div className="card-body">
                     <h2 className="card-title">{name}</h2>
                     <p>{details}</p>
